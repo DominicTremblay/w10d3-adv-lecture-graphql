cb51bf23b4fc6b5eef1a4296683664f81a59d78a
Dominic Tremblay <dominictremblay24@gmail.com>
2019-10-23 01:34:31 -0400

    added catgory to todos resolver

--- src/api/todos/todos.resolver.js
+++ src/api/todos/todos.resolver.js
@@ -6,4 +6,17 @@ module.exports = {
       return result.rows;
     },
   },
+  Todo: {
+    category: async(parent, args, context, info) => {
+      console.log('todo-category resolver', parent.id);
+      const query = {
+        text:
+          'select categories.id, categories.category from todos inner join categories on categories.id = todos.category_id where todos.id = $1',
+        values: [parent.id],
+      };
+
+      const result = await context.db.query(query);
+      return result.rows[0];
+    },
+  },
 };
